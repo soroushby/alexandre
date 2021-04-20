ae692d5bfc7a0d1805e96a4efaaad6d87bd8d632
soroushby <49848393+soroushby@users.noreply.github.com>
2021-04-20 06:22:04 -0700

    updated portfolio components

--- src/app/portflio/bakery/bakery.component.ts
+++ src/app/portflio/bakery/bakery.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ProjectData } from 'src/app/interfaces/project-data';
@@ -11,7 +12,7 @@ import { DataService } from 'src/app/services/data.service';
 })
 export class BakeryComponent implements OnInit {
   data: Observable<ProjectData[]> | undefined;
-  constructor(private dataService: DataService) {}
+  constructor(private dataService: DataService, public auth: AuthService) {}
 
   ngOnInit(): void {
     this.data = this.dataService.gotData.pipe(
